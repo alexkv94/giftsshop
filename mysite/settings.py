b6d540cb482d3ddf1fc33ed0cdccc90cd0cbd9d7
alexkv94 <101505335+alexkv94@users.noreply.github.com>
2023-04-17 11:13:08 +0100

    added app for store

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'giftzone',
     'users',
+    'store',
 ]
 
 MIDDLEWARE = [
---
+++ store/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50)
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.title
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
