3dab5c4f28f8c00deebf502682aee28e37890df1
alexkv94 <101505335+alexkv94@users.noreply.github.com>
2023-05-05 23:31:18 +0100

    changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,7 +31,7 @@ LOGOUT_REDIRECT_URL = 'home'
 CART_SESSION_ID = 'cart'
 SESSION_COOKIE_AGE = 259200
 
-ALLOWED_HOSTS = ['giftzone-service.onrender.com']
+ALLOWED_HOSTS = ['giftzone-service.onrender.com', '127.0.0.1']
 
 
 # Application definition
--- store/management/commands/parse_data.py
+++ store/management/commands/parse_data.py
@@ -1,19 +1,22 @@
 import csv
 from faker import Faker
 from django.utils.text import slugify
-import os
 from pathlib import Path
-from django.db import models
+from decimal import Decimal
+from django.core.exceptions import ValidationError
 from django.core.management.base import BaseCommand, CommandError
 from store.models import Category, Product
+from decimal import Decimal, InvalidOperation
 
-# CSV file is location
-csv_filepath = '/CSVData/datashortened.csv.csv'
+
+# CSV file location
+csv_filepath = 'store/CSVData/datashortened.csv.csv'
 
 fake_description = Faker()
 
 class Command(BaseCommand):
     help = 'Load data from csv'
+
     def handle(self, *args, **options):
         # drop the data from the table so that if we rerun the file, we don't repeat values
         Category.objects.all().delete()
@@ -22,14 +25,14 @@ class Command(BaseCommand):
         print("table dropped successfully")
 
         # create table again
-        # open the file to read it into th database
+        # open the file to read it into the database
         base_dir = Path(__file__).resolve().parent.parent.parent.parent
 
-        with open(str(base_dir) + '/CSVData/datashortened.csv', newline='') as csvfile:
+        with open(str(base_dir) + '/store/CSVData/datashortened.csv', newline='') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
                 # Create category based on the country of origin
-                category, _ = Category.objects.get_or_create(
+                category, created = Category.objects.get_or_create(
                     title=row['Country'],
                     slug=slugify(row['Country'])
                 )
@@ -37,12 +40,22 @@ class Command(BaseCommand):
                 # Create a fake description using the Faker library
                 description = fake_description.paragraph()
 
+                # Convert price to a decimal
+                try:
+                    price = Decimal(row['UnitPrice'])
+                except InvalidOperation:
+                    price = Decimal('0.0')
+
                 # adding products into the database
-                product = Product.objects.create(
-                    category=category,
-                    title=row['Description'],
-                    slug=slugify(row['title']),
-                    description=description,
-                    price=row['UnitPrice'],
-                    image=None
-                )
+                try:
+                    product = Product.objects.create(
+                        category=category,
+                        title=row['Description'],
+                        slug=slugify(row['Description']),
+                        description=description,
+                        price=price,
+                        image=None
+                    )
+                except ValidationError as e:
+                    print(f"Error creating product {row['Description']}: {e.message_dict}")
+            print('added data to tables')
