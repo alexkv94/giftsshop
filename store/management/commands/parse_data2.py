ed75ceaef1549439d09c1079a263f081bd13928b
alexkv94 <101505335+alexkv94@users.noreply.github.com>
2023-05-06 17:18:56 +0100

    deleted csv

--- store/management/commands/parse_data2.py
+++ store/management/commands/parse_data2.py
@@ -2,15 +2,11 @@ import csv
 from faker import Faker
 from django.utils.text import slugify
 from pathlib import Path
-from decimal import Decimal
 from django.core.exceptions import ValidationError
 from django.core.management.base import BaseCommand, CommandError
 from store.models import Category, Product
 from decimal import Decimal, InvalidOperation
 
-
-# CSV file location
-csv_filepath = 'store/CSVData/datashortened.csv.csv'
 
 fake_description = Faker()
 
@@ -28,38 +24,58 @@ class Command(BaseCommand):
         # create table again
         # open the file to read it into the database
         base_dir = Path(__file__).resolve().parent.parent.parent.parent
+        with open(str(base_dir) + '/store/CSVData/datashortened.csv', newline='') as f:
+            reader = csv.reader(f, delimiter=',')
 
-        with open(str(base_dir) + '/store/CSVData/datashortened.csv', newline='') as csvfile:
-            reader = csv.DictReader(csvfile)
             n = 0
-            count = 0
-            for row in reader:
-                # Create category based on the country of origin
-                category, created = Category.objects.get_or_create(
-                    title=row['Country'],
-                    slug=slugify(row['Country'])
-                )
+            m = 0
+            next(reader) #skip header
 
-                # Create a fake description using the Faker library
+            categories = set()
+            for row in reader:
+                categories.add(row[7])
+
+                n+=1
+                print(f'{n} iterations completed in first loop')
+
+
+            for unique_category in categories:
+                if unique_category:
+
+                    category = Category.objects.create(
+                        title=unique_category,
+                        slug = slugify(unique_category)
+
+                    )
+                    category.save()
+            print('completed insertion to "category table"')
+
+            f.seek(0)
+            next(reader)  # skip header
+            for row in reader:
                 product_description = fake_description.paragraph()
 
-                # Convert price to a decimal
                 try:
                     price = Decimal(row['UnitPrice'])
                 except InvalidOperation:
                     price = Decimal('0.0')
 
-                # adding products into the database
-                try:
+                if row[7]:
+
                     product = Product.objects.create(
-                        category=category,
-                        title=row['Description'],
-                        slug=slugify(row['Description']),
+                        category=row[7],
+                        title=row[2],
+                        slug=slugify(row[2]),
                         description=product_description,
                         price=price,
                         image=None
                     )
-                except ValidationError as e:
-                    print(f"Error creating product {row['Description']}: {e.message_dict}")
+                product.save()
 
-            print('added data to tables')
+            m += 1
+            print(f'{m} iterations completed for second table')
+
+
+
+
+
